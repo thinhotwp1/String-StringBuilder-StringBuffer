1d45d3fe8989b8e9ae4ac8ab07f464c45a3f8362
ThinhLD <thinhld@telsoft.com.vn>
2025-03-25 11:01:46 +0700

    init

--- base_project/src/main/java/ldt/demo/base_project/StringCoach.java
+++ base_project/src/main/java/ldt/demo/base_project/StringCoach.java
@@ -9,16 +9,15 @@ public class StringCoach {
         String b = "String";
         String c = new String("String");
 
-        // == compare address of objects
+        System.out.println("1. '==' compare address of 2 objects");
         System.out.println("a == b: " + (a == b)); // true because same address in String Interning - String Pools
         System.out.println("a == c: " + (a == c)); // false because create new String -> different object address
 
-        // equals compare value of objects
+        System.out.println("2. 'equals' compare value of 2 objects");
         System.out.println("a equals b: " + a.equals(b)); // true because same value
         System.out.println("a equals c: " + a.equals(c)); // true because same value
 
-        // Put String c to String Interning - String Pools and compare again
-        System.out.println("Put c into String Interning - String Pools...");
+        System.out.println("3. Put String c into String Interning - String Pools and compare with a again");
         c = c.intern();
         System.out.println("a == c: " + (a == c)); // true because same address in String Pools
     }
