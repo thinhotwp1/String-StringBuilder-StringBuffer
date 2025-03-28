8eec04187e34dd37527e5fa42fc384be51abfd9f
ThinhLD <thinhld@telsoft.com.vn>
2025-03-28 16:48:36 +0700

    init

--- base_project/src/main/java/ldt/demo/base_project/StringCoach.java
+++ base_project/src/main/java/ldt/demo/base_project/StringCoach.java
@@ -5,21 +5,27 @@ public class StringCoach {
      * String using String Iterning (String Pools) to performance system, where String create and store in a string pool, view example to clearly
      */
     public static void main(String[] args) {
+        System.out.println("""
+                Input:
+                String a = "String";
+                String b = "String";
+                String c = new String("String");
+                """);
         String a = "String";
         String b = "String";
         String c = new String("String");
 
-        System.out.println("1. '==' compare address of 2 objects");
-        System.out.println("a == b: " + (a == b)); // true because same address in String Interning - String Pools
-        System.out.println("a == c: " + (a == c)); // false because create new String -> different object address
+        System.out.println("1. '==' will compare address of 2 objects");
+        System.out.println("    a == b: " + (a == b)); // true because same address in String Interning - String Pools
+        System.out.println("    a == c: " + (a == c)); // false because create new String -> different object address
 
-        System.out.println("2. 'equals' compare value of 2 objects");
-        System.out.println("a equals b: " + a.equals(b)); // true because same value
-        System.out.println("a equals c: " + a.equals(c)); // true because same value
+        System.out.println("2. 'equals' will compare value of 2 objects");
+        System.out.println("    a equals b: " + a.equals(b)); // true because same value
+        System.out.println("    a equals c: " + a.equals(c)); // true because same value
 
-        System.out.println("3. Put String c into String Interning - String Pools and compare with a again");
+        System.out.println("3. Put String c into String Interning - String Pools and compare address with a again");
         c = c.intern();
-        System.out.println("a == c: " + (a == c)); // true because same address in String Pools
+        System.out.println("    a == c: " + (a == c)); // true because same address in String Pools
     }
 
 }
